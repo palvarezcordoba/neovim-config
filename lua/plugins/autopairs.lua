f416c3b9b91a812dc8eeab231a52df00afed1187
Álvarez Córdoba,Pablo,CH-Vevey <Pablo.AlvarezCordoba@nestle.com>
2024-09-29 16:35:40 +0200

    disable autopair in some filetypes

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -6,8 +6,12 @@ return {
   event = 'InsertEnter',
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
-    require('nvim-autopairs').setup {}
+  opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+  },
+  config = function(_, opts)
+    require('nvim-autopairs').setup(opts)
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
